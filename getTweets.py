d26a8d10c30924687e16c98c291e5bc56b1e5144
rmys.guc <rmys.guc@hotmail.com>
2023-05-04 16:50:13 +0300

    bugfix2

--- getTweets.py
+++ getTweets.py
@@ -1,48 +1,41 @@
-import configparser
 import tweepy
 import pandas as pd
-# Read the config file
+import pytz
+import datetime
 
-from openpyxl import Workbook
-
-config = configparser.RawConfigParser()
-config.read('config.ini')
-
-api_key = "ukC1eP3Epq43gIYwbBq4knLXF"
-api_key_secret = "e6uTMsSgual31w5EfR1n5kfvN9ZqKoj15WgoPLsk0BBD8LZDhX"
+# Twitter API bilgileri
+consumer_key = "ukC1eP3Epq43gIYwbBq4knLXF"
+consumer_secret = "e6uTMsSgual31w5EfR1n5kfvN9ZqKoj15WgoPLsk0BBD8LZDhX"
 access_token = "2815936960-mS7TT9j5uQ5B12Zfr5rS1nd5E0Ugsh0HZSODjN4"
 access_token_secret = "H2P2ZyIM1G2Nt8dwBZvpiuBNrr1VBOC72BZvRyhyB9nm1"
-# Read the values
-#api_key = config['twitter']['api_key']
-#api_key_secret = config['twitter']['api_key_secret']
-#bearer_token = "AAAAAAAAAAAAAAAAAAAAABqwnAEAAAAA6E9eil5dENcQoGs8NS3M6sxqlzc%3DSRRwQ1J7CCZgjUJ3pO2IrcpGVV4K4EYJUUVM6jkPRliBPuIuM5"
-#access_token = config['twitter']['access_token']
-#access_token_secret = config['twitter']['access_token_secret']
-auth = tweepy.OAuthHandler(api_key,api_key_secret)
-auth.set_access_token(access_token,access_token_secret)
 
-api = tweepy.API(auth)
-limit =10
-hashtag = "istanbulsozlesmesi"
+# Tweepy API objesi oluşturma
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-columns = ['User', 'Tweet', 'Likes', 'Retweets', 'Date']
+# Aranacak kelimeleri ve tarih aralığını belirleme
+search_words = ['python', 'kod']
+query = ' OR '.join(search_words)
+since_date = datetime.datetime(2022, 5, 1, 0, 0, 0, tzinfo=pytz.UTC)
+until_date = datetime.datetime(2022, 5, 4, 0, 0, 0, tzinfo=pytz.UTC)
+
+# Tweetleri çekme
+tweets = []
+for tweet in tweepy.Cursor(api.search_tweets, q=query, lang='tr', count=100).items():
+    if tweet.created_at < since_date:
+        break
+    if tweet.created_at > until_date:
+        continue
+    if tweet.in_reply_to_status_id is not None:
+        continue
+    tweets.append(tweet)
+
+# Tweetleri Pandas DataFrame'e dönüştürme
 data = []
-
-try:
-    api.verify_credentials()
-    print('Successful Authentication')
-    try:
-        tweets = tweepy.Cursor(api.search_tweets, q=hashtag, lang = "tr", count=15).pages(5)
-        #print(type(tweets))
-        
-        for tweet in tweets:
-            print(tweet.id)
-          
-
-    except Exception as ex:
-        print(ex)
-except:
-    print('Failed authentication')
-
-df = pd.DataFrame(data, columns=columns)
-df.to_excel("Cup2022.xlsx")
+for tweet in tweets:
+    data.append([tweet.user.name, tweet.user.location, str(tweet.created_at.date()), tweet.text, tweet.user.verified,
+                 tweet.retweet_count, tweet.favorite_count, tweet.source])
+df = pd.DataFrame(data, columns=['username', 'location', 'created_at', 'tweet', 'blueTick', 'retweetCount',
+                                 'favoriteCount', 'source'])
+print(df.head())
--- test2.py
+++ test2.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json
 import datetime
 from json import JSONEncoder
+import pytz
 
 
 class DateTimeEncoder(JSONEncoder):
@@ -26,18 +27,32 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 #####United Airlines
 # Open/Create a file to append data
 data =[]
-queryTopics = 'istanbul sözleşmesi or "kadın hakları"'
-for tweet in tweepy.Cursor(api.search_tweets,q=queryTopics,count=100,
-                           lang="tr",
-                           since="2017-04-03").items():
-    data.append([ tweet.user.name, 
-                                                tweet.user.location,\
-                                               str(tweet.created_at.date()),
-                                            tweet.text,
-                                            tweet.retweet_count,
-                                            tweet.favorite_count, 
-                                             tweet.source])
-    print (tweet.created_at, tweet.text)
-df = pd.DataFrame(data, columns=['username','location','created_at','tweet','retweetCount','favoriteCount','source'])
+
+
+turkey = pytz.timezone('Europe/Istanbul')
+
+since_id = turkey.localize(datetime.datetime(2019, 1, 1, 0, 0, 0))
+until_id = turkey.localize(datetime.datetime(2020, 1, 31, 23, 59, 59))
+search_words = ['python', 'kod']
+query = ' OR '.join(search_words) + ' -filter:retweets'
+data = []
+for tweet in tweepy.Cursor(api.search, q=query, count=100, lang='tr', tweet_mode='extended', since_id=since_id, until_id=until_id).items():
+    if tweet.created_at < since_id:
+        break
+    if tweet.created_at > until_id:
+        continue
+    if tweet.in_reply_to_status_id is not None:
+        continue
+    data.append([tweet.user.name, 
+                 tweet.user.location,
+                 str(tweet.created_at.date()),
+                 tweet.full_text, 
+                 tweet.user.verified,
+                 tweet.retweet_count,
+                 tweet.favorite_count, 
+                 tweet.source])
+    print(tweet.created_at, tweet.full_text)
+df = pd.DataFrame(data, columns=['username','location','created_at','tweet','blueTick','retweetCount','favoriteCount','source'])
 print(df.head())
-df.to_excel("all2.xlsx")
+filename = str(created_on.date()) +'.xlsx'
+df.to_excel(filename)
